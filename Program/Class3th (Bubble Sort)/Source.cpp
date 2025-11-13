00fe917c34c09930214119073515855b522d18f5
tgahn04 <tgahn0524@gmail.com>
2025-11-13 19:41:46 +0900

    (2025.11.13) 거품 정렬

---
+++ Program/Class3th (Bubble Sort)/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 인플레이스 정렬
+	// 정렬 과정에서 추가적인 자료구조 또는 메모리 공간을 많이 사용하지 않고,
+	// 원래 주어진 배열이나 리스트 자체를 직접 수정하여 정렬하는 방식입니다.
+#pragma endregion
+
+#pragma region 거품 정렬
+	// 서로 인접한 두 원소를 비교하여 정렬하는 알고리즘입니다.
+
+	int size[] = { 7, 4, 5, 1, 3 };
+	int n = sizeof(size) / sizeof(size[0]);
+	int temporary;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < (n - i) - 1; j++)
+		{
+			if (size[j] > size[j + 1])
+			{
+				swap(size[j], size[j + 1]);
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << size[i] << " ";
+	}
+#pragma endregion
+
+	return 0;
+}
---
+++ Program/Class4th(Select Sort)/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 선택 정렬
+	// 주어진 리스트 중에 최소값을 찾은 다음 특정한 위치에서
+	// 결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+
+	int list[] = { 9, 6, 7, 3, 5, 8, 2, 1, 4, 11, 15 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		int min_index = i;
+
+		for (int j = i + 1; j < size; j++)
+		{
+			if (list[j] < list[min_index])
+			{
+				min_index = j;
+			}
+		}
+
+		swap(list[i], list[min_index]);
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << list[i] << " ";
+	}
+#pragma endregion
+
+
+	return 0;
+}
