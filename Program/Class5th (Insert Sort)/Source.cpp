0c666f550ee831b026fb7604a742ca246a9712ec
tgahn04 <tgahn0524@gmail.com>
2025-11-14 19:44:09 +0900

    (2025.11.14) 선택 정렬

---
+++ Program/Class5th (Insert Sort)/Source.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 삽입 정렬
+	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
+	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다.
+
+	int list[] = { 8, 5, 6, 2, 4, 7, 1, 3, 9, 10 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+	for (int i = 0; i < size; i++)
+	{
+		int key = list[i];
+		int index = i - 1;
+
+		while (index >= 0 && key < list[index])
+		{
+			list[index + 1] = list[index];
+
+			index--;
+		}
+
+		list[index + 1] = key;
+	}
+
+	for (const auto& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 int main()
 {
-#pragma region 
+#pragma region 유클리드 호제법
+	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다,
 
+	int x = 2354;
+	int y = 3326;
+	int value = 0;
+
+	for (int i = 1; i <= x && i <= y; i++)
+	{
+		if (x % i == 0 && y % i == 0)
+		{
+			value = i;
+		}
+	} 
+
+	cout << "x, y의 최대 공약수 : " << value << endl;
 
 
 #pragma endregion
